33da8e55ff67467e2c7a40edff44b2ca58a796ff
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-03-22 16:51:08 +0300

    Add hunter-toolchain-info.rb

---
+++ hunter-toolchain-info.rb
@@ -0,0 +1,32 @@
+require 'formula'
+
+class HunterToolchainInfo < Formula
+  version '0.0.1'
+
+  homepage 'https://github.com/hunter-packages/toolchain-info-mini'
+  url 'https://github.com/hunter-packages/toolchain-info-mini', :branch => 'master'
+
+  devel do
+    version '0.0.1'
+    url 'https://github.com/hunter-packages/toolchain-info-mini', :branch => 'master'
+  end
+
+  depends_on 'cmake' => :build
+
+  def install
+    args = *std_cmake_args
+
+    if build.with? "debug"
+      args << "-DCMAKE_BUILD_TYPE=Debug"
+    else
+      args << "-DCMAKE_BUILD_TYPE=Release"
+    end
+
+    args << "-DHUNTER_STATUS_DEBUG=ON"
+
+    system "cmake", *args
+    system "make"
+    system "make", "install"
+
+  end
+end
