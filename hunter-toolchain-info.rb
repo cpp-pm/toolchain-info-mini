19f2bdef179961877beaa4d479794904c4860aee
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-03-22 17:21:44 +0300

    Fix URL: Add '.git'

--- hunter-toolchain-info.rb
+++ hunter-toolchain-info.rb
@@ -4,11 +4,11 @@ class HunterToolchainInfo < Formula
   version '0.0.1'
 
   homepage 'https://github.com/hunter-packages/toolchain-info-mini'
-  url 'https://github.com/hunter-packages/toolchain-info-mini', :branch => 'master'
+  url 'https://github.com/hunter-packages/toolchain-info-mini.git', :branch => 'master'
 
   devel do
     version '0.0.1'
-    url 'https://github.com/hunter-packages/toolchain-info-mini', :branch => 'master'
+    url 'https://github.com/hunter-packages/toolchain-info-mini.git', :branch => 'master'
   end
 
   depends_on 'cmake' => :build
